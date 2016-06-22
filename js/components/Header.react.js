2fe1f3b28c7a351a463dcd8b9d27aec7549f745a
Raman Alesiankou <ralesiankou@klika-tech.com>
2016-06-23 00:41:31 +0300

    Refactoring: rewrite 'Header' component as a pure function

--- js/components/Header.react.js
+++ js/components/Header.react.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 
-export default class Header extends Component {
+const Header = function () {
 
-  render() {
-    return (
-      <div>
-        <header id="header">
-          <h1>Contacts App</h1>
-        </header>
-        <ul role="nav" className="header-nav">
-          <li><Link to="/" onlyActiveOnIndex activeClassName="active" className="link">Home</Link></li>
-          <li><Link to="/contacts" activeClassName="active" className="link">Contacts</Link></li>
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <header id="header">
+        <h1>Contacts App</h1>
+      </header>
+      <ul role="nav" className="header-nav">
+        <li><Link to="/" onlyActiveOnIndex activeClassName="active" className="link">Home</Link></li>
+        <li><Link to="/contacts" activeClassName="active" className="link">Contacts</Link></li>
+      </ul>
+    </div>
+);
 
 }
+
+export {Header as default};
