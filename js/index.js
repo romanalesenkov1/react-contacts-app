6e7a79ffc8e9f561430246bccfc951f219e7b593
Raman Alesiankou <ralesiankou@klika-tech.com>
2016-06-26 19:59:29 +0300

    Router; remove hash from url

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
 
 import App from './components/App.react';
 import Home from './components/Home.react';
 import ContactApp from './components/ContactApp.react';
 import Profile from './components/Profile.react';
 
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
+
 ReactDOM.render((
-  <Router history={hashHistory}>
+  <Router history={appHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
       <Route path="/contacts" component={ContactApp} />
